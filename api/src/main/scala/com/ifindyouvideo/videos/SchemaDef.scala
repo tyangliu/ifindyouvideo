bfc17a289d05cf8f672420e1c0d2784c1783ff12
tyangliu <tyangliu@gmail.com>
2015-10-22 14:11:59 -0700

    Add location and channel to sangria schema

--- api/src/main/scala/com/ifindyouvideo/videos/SchemaDef.scala
+++ api/src/main/scala/com/ifindyouvideo/videos/SchemaDef.scala
@@ -4,25 +4,67 @@ import sangria.schema._
 import scala.concurrent.Future
 
 object SchemaDef {
+  val Location = ObjectType(
+    "Location",
+    "A Location containing lat, long, and altitude",
+    fields[Unit, Location](
+      Field("latitude", BigDecimalType,
+        Some("The latitude coordinate"),
+        resolve = _.value.latitude
+      ),
+      Field("longitude", BigDecimalType,
+        Some("The longitude coordinate"),
+        resolve = _.value.longitude
+      ),
+      Field("altitude", BigDecimalType,
+        Some("The altitude coordinate"),
+        resolve = _.value.altitude
+      )
+    )
+  )
+
+  val Channel = ObjectType(
+    "Channel",
+    "A YouTube channel",
+    fields[Unit, Channel](
+      Field("id", StringType,
+        Some("The channel id"),
+        resolve = _.value.id
+      ),
+      Field("title", StringType,
+        Some("The channel title"),
+        resolve = _.value.title
+      )
+    )
+  )
+
   val Video = ObjectType(
     "Video",
     "A YouTube video",
     fields[Unit, Video](
       Field("id", StringType,
-        Some("The id of the video."),
+        Some("The id of the video"),
         resolve = _.value.id
       ),
       Field("title", StringType,
-        Some("The title of the video."),
+        Some("The title of the video"),
         resolve = _.value.title
       ),
       Field("description", OptionType(StringType),
-        Some("The description of the video."),
+        Some("The description of the video"),
         resolve = _.value.description.getOrElse("")
       ),
       Field("tags", ListType(StringType),
-        Some("The video's tags."),
+        Some("The video's tags"),
         resolve = _.value.tags
+      ),
+      Field("location", Location,
+        Some("The video's recording location"),
+        resolve = _.value.location
+      ),
+      Field("channel", Channel,
+        Some("The video's channel"),
+        resolve = _.value.channel
       )
     )
   )
--- api/src/main/scala/com/ifindyouvideo/videos/videos.scala
+++ api/src/main/scala/com/ifindyouvideo/videos/videos.scala
@@ -3,27 +3,27 @@ package com.ifindyouvideo.videos
 import scala.concurrent.Future
 import com.websudos.phantom.dsl._
 
-case class Video(
-  id:          String,
-  title:       String,
-  description: Option[String],
-  tags:        List[String]/*,
-  location:    Location,
-  channel:     Channel,
-  thumbnails:  Thumbnails?*/
-)
-
-case class Location(latitude: Double, longitude: Double, altitude: Double)
+case class Location(latitude: BigDecimal, longitude: BigDecimal, altitude: BigDecimal)
 case class Channel(id: String, title: String)
 
 case class Image(url: String, width: Int, height: Int)
 
 case class Thumbnails(
-  default:  Image,
-  medium:   Image,
-  high:     Image,
-  standard: Image,
-  maxres:   Image
+   default:  Image,
+   medium:   Image,
+   high:     Image,
+   standard: Image,
+   maxres:   Image
+ )
+
+case class Video(
+  id:          String,
+  title:       String,
+  description: Option[String],
+  tags:        List[String],
+  location:    Location,
+  channel:     Channel,
+  thumbnails:  Option[Thumbnails]
 )
 
 class VideoRepo {
@@ -34,19 +34,32 @@ class VideoRepo {
 
 object VideoRepo {
   val videos = List(
-
     Video(
-      id="1000",
-      title="Test",
-      description=Some("test"),
-      tags=List("tag1", "tag2")
+      id = "1000",
+      title = "Awesome Video",
+      description = Some("test"),
+      tags = List("tag1", "tag2"),
+      location = Location(5.3289,5.394,3.239),
+      channel = Channel("1234", "A Channel Title"),
+      thumbnails = None
     ),
     Video(
-      id="1001",
-      title="Video Title",
-      description=Some("test3"),
-      tags=List("tag3", "tag4")
+      id = "1001",
+      title = "Test",
+      description = Some("test 2"),
+      tags = List("tag3", "tag4"),
+      location = Location(5.3289,5.394,3.239),
+      channel = Channel("1234", "A Channel Title"),
+      thumbnails = None
+    ),
+    Video(
+      id = "1002",
+      title = "Another Test",
+      description = Some("test 3"),
+      tags = List("tag5", "tag6"),
+      location = Location(5.3289,5.394,3.239),
+      channel = Channel("1234", "A Channel Title"),
+      thumbnails = None
     )
-
   )
 }
