9bd06fe13b68ae32de5c6a1bdc6c1149f64396d5
llardizabal <liannelardizabal@gmail.com>
2015-11-01 17:44:01 -0800

    Started VideoCard component

---
+++ client/app/components/VideoCard.jsx
@@ -0,0 +1,29 @@
+'use strict';
+
+import React, { Component } from 'react';
+import Radium from 'radium';
+import styler from 'react-styling';
+
+static defaultProps = {
+    title: "Vancouver",
+    mapId: 35,
+    thumbnailURL: "testURL",
+    views: 100,
+    likes: 500
+};
+
+@Radium
+export default class VideoCard extends Component {
+    render() {
+        let video = this.props.video;
+        return {
+            <div>
+                <div>(video.title)</div>
+                <div>(video.mapId)</div>
+                <div>(video.thumbnailUrl)</div>
+                <div>(video.views)</div>
+                <div>(video.likes)</div>
+            </div>
+        };
+    }
+}
