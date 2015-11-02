86f9b899aa1f87dad53a08ae4618c085a508ff51
Andrea Yeo <ayeo@dhcp-128-189-82-237.ubcsecure.wireless.ubc.ca>
2015-11-01 21:18:49 -0800

    Add styling VideoOverlay component

--- client/app/components/VideoOverlay.jsx
+++ client/app/components/VideoOverlay.jsx
@@ -4,8 +4,6 @@ import React, { Component } from 'react';
 import Radium from 'radium';
 import styler from 'react-styling';
 
-
-
 @Radium
 export default class VideoOverlay extends Component {
 
@@ -18,11 +16,54 @@ export default class VideoOverlay extends Component {
 
   render() {
     return (
-      <div>
-        <img src="../images/logo-white.svg"/>
-          <span>{this.props.video.title}</span>
-          <span>{this.props.video.mapId}</span>
+      <div style={styles.videoOverlay}>
+        <div style={styles.logoImg}></div>
+        <div style={styles.contentBox}>
+          <p style={styles.mapId}>{this.props.video.mapId}</p>
+          <p style={styles.title}>{this.props.video.title}</p>
+        </div>
+        <div style={styles.clearfix} />
       </div>
     );
   }
 }
+
+const styles = styler`
+  videoOverlay
+    position: relative
+
+  logoImg
+    background: url(${require('../images/logo-red-outline-shadow.svg')}) no-repeat center;
+    background-size: 65px
+    width: 50px
+    height: 65px
+    margin-top: -5px
+    position: absolute
+    left: 0
+    z-index: 10
+
+  contentBox
+    background: rgba(255,255,255,1)
+    margin-left: 15px
+    margin-top: 10px
+    box-shadow: 0px 0px 2px 1px rgba(180,180,180,1)
+    float: left
+
+  mapId
+    display: inline-block
+    border-right: 1px solid rgba(180,180,180,1)
+    line-height: 30px
+    padding: 0 10px 0 35px
+    color: rgba(239,48,81,1)
+
+  title
+    display: inline-block
+    line-height: 30px
+    padding: 0 10px
+    font-weight: 700
+    color: rgba(102,102,102,1)
+
+  clearfix
+    clear: both
+    display: table
+`;
--- client/app/containers/Test.jsx
+++ client/app/containers/Test.jsx
@@ -4,13 +4,16 @@ import React, { Component } from 'react';
 import DocumentTitle from 'react-document-title';
 import Radium from 'radium';
 import styler from 'react-styling';
-
+import VideoOverlay from '../components/VideoOverlay.jsx'
 @Radium
 export default class Test extends Component {
 
   render() {
     return (
+      <div>
         <div>Testing</div>
+        <VideoOverlay/>
+      </div>
     );
   }
 
