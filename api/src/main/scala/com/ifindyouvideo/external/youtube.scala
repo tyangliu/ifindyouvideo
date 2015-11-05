3d4c7c82cb11624d87bf9cd105433fa4e60e1759
tyangliu <tyangliu@gmail.com>
2015-11-04 16:22:35 -0800

    Add code to extract list of ids out of youtube response

--- api/src/main/scala/com/ifindyouvideo/external/youtube.scala
+++ api/src/main/scala/com/ifindyouvideo/external/youtube.scala
@@ -1,22 +1,24 @@
 package com.ifindyouvideo.external
 
-import org.json4s.JsonAST.JValue
-
 import scala.concurrent.Future
 import scala.util.{Success, Failure}
 import java.io.IOException
+
 import akka.actor._
 import akka.stream.ActorMaterializer
+
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.unmarshalling._
-import scala.async.Async.{async, await}
-import org.json4s.native.JsonMethods._
-import org.json4s._
-import com.ifindyouvideo.videos._
 
-import de.heikoseeberger.akkahttpjson4s.Json4sSupport._
+import scala.async.Async.{async, await}
+
+import org.json4s.native.JsonMethods._
+import org.json4s.native.Serialization
+import org.json4s._
+
+import com.ifindyouvideo.videos._
 
 object YoutubeProtocol {
   case class Search(location: Location, radius: String)
@@ -26,8 +28,11 @@ class YoutubeService extends Actor {
 
   import YoutubeProtocol._
   import scala.concurrent.ExecutionContext.Implicits.global
+  import de.heikoseeberger.akkahttpjson4s.Json4sSupport._
 
   implicit val materializer = ActorMaterializer()
+  implicit val serialization = Serialization
+  implicit val formats = DefaultFormats
 
   def receive = {
     case Search(location, radius) => search(location, radius)
@@ -48,18 +53,21 @@ class YoutubeService extends Actor {
     )
 
     val req = HttpRequest(uri = Uri("https://www.googleapis.com/youtube/v3/search").withQuery(params))
-    val res: HttpResponse = await { Http(context.system).singleRequest(req) }
+    val res = await { Http(context.system).singleRequest(req) }
 
-    println(res)
+    res.status match {
+      case OK => Unmarshal(res.entity).to[JValue] map { resJson =>
+        val ids = (resJson \ "items" \ "id" \ "videoId").values
+        println(ids)
+      }
+      /*
+      case BadRequest => Left("Invalid Request")
+      case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+        val error = s"YouTube request failed with status code ${response.status} and entity $entity"
+        new IOException(error)
+      }
+      */
+    }
   } onFailure { case e => println("An error has occured: " + e.getMessage) }
-/*
-  map { response => response.status match {
-   case OK => Unmarshal(response.entity).to[JValue]
-   case BadRequest => Left("Invalid Request")
-   case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
-     val error = s"YouTube request failed with status code ${response.status} and entity $entity"
-     new IOException(error)
-   }
- }}*/
 
 }
