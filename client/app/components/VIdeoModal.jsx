48b309e9f0128b221561ead893a437682c87e2b3
rogermyang <rogermyang@hotmail.com>
2015-11-05 00:20:11 -0800

    add video Modal componet

--- client/app/components/VIdeoModal.jsx
+++ client/app/components/VIdeoModal.jsx
@@ -5,7 +5,7 @@ import Radium from 'radium';
 import styler from 'react-styling';
 
 @Radium
-export default class UserWidget extends Component {
+export default class V extends Component {
 
   render() {
     let video = this.props.video;
@@ -24,31 +24,5 @@ export default class UserWidget extends Component {
 }
 
 const styles = styler`
-  avatar
-    width: 26px
-    height: 26px
-    border-radius: 1000px
-    background: rgba(255,255,255,1)
-    float: left
-    margin-right: 10px
-    margin-top: -2px
 
-  nameDropdown
-    float: left
-    color: rgba(255,255,255,1)
-
-  name
-    line-height: 26px
-    font-size: 14px
-    font-weight: 700
-    float: left
-
-  dropdownIcon
-    line-height: 26px
-    font-size: 20px
-    margin-left: 6px
-
-  clearfix
-    clear: both
-    display: table
 `;
