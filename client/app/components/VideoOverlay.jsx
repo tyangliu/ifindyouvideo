4edb4fd0e226028b333b2dcafe4114e733ab7951
Alan Wong <wong-alan@users.noreply.github.com>
2015-11-06 17:41:26 -0800

    Fixed overlay, again.

--- client/app/components/VideoOverlay.jsx
+++ client/app/components/VideoOverlay.jsx
@@ -27,7 +27,7 @@ export default class VideoOverlay extends Component {
     return (
       <div style={styles.VideoOverlay} onClick={this.handleClick}>
         <div style={styles.logoImg[this.state.selected ? 'selected' : 'normal']}></div>
-        <div style={styles.contentBox[this.state.selected ? 'normal' : 'selected']}>
+        <div style={styles.contentBox[this.state.selected ? 'selected' : 'normal']}>
           <span style={styles.mapId}>{this.props.video.mapId}</span>
           <span style={styles.title}>{this.props.video.title}</span>
         </div>
