69d51e08cdda4038c4f1331d6185e6672e4d8f31
tyangliu <tyangliu@gmail.com>
2015-11-11 19:24:39 -0800

    Hook up database class to our temp remote cassandra instance, write method to generate tables

--- api/src/main/scala/com/ifindyouvideo/database/database.scala
+++ api/src/main/scala/com/ifindyouvideo/database/database.scala
@@ -1,15 +1,24 @@
 package com.ifindyouvideo.database
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import com.websudos.phantom.db.DatabaseImpl
 import com.websudos.phantom.connectors._
 
 class Database(val keyspace: KeySpaceDef) extends DatabaseImpl(keyspace) {
+
   object videos extends ConcreteVideoTable with keyspace.Connector
   object videosByGeohash extends ConcreteVideoByGeohashTable with keyspace.Connector
+
+  def createTables = {
+    Await.result(videos.create.ifNotExists().future(), 5000 millis)
+    Await.result(videosByGeohash.create.ifNotExists().future(), 5000 millis)
+  }
+
 }
 
 object Connector {
-  val hosts = Seq("159.23.23.129")
+  val hosts = Seq("162.243.198.248")
   val connector = ContactPoints(hosts).keySpace("ifindyouvideo")
 }
 
