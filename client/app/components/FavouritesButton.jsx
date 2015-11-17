62e0b24a74238b3cf50440f108e8f04445aa4ed2
llardizabal <liannelardizabal@gmail.com>
2015-11-17 02:09:22 -0800

    Initialize Favourites Button component

---
+++ client/app/components/FavouritesButton.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React, { Component } from 'react';
+import Radium from 'radium';
+import styler from 'react-styling';
+import config from '../config.js';
+
+@Radium
+export default class FavouritesButton extends Component {
+    
+}
